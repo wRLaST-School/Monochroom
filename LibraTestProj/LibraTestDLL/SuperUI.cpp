25fae2e245896401eda8f63b211797e70d6c4df3
Wrelf <elf@wrelf.net>
2024-06-20 13:07:59 +0900

    なんかSuperUIがエラー吐くので一旦コメントアウト

--- LibraTestProj/LibraTestDLL/SuperUI.cpp
+++ LibraTestProj/LibraTestDLL/SuperUI.cpp
@@ -1,25 +1,25 @@
-#include "SuperUI.h"
-#include <Camera.h>
-
-
-void SuperUI::Init()
-{
-	// 現在のカメラ座標を代入
-	mUIParentObj.position = Camera::sCurrent->position;
-
-}
-
-void SuperUI::Update()
-{
-
-}
-
-void SuperUI::Draw()
-{
-
-}
-
-void SuperUI::CopyComponent(IComponent* src)
-{
-
-}
+//#include "SuperUI.h"
+//#include <Camera.h>
+//
+//
+//void SuperUI::Init()
+//{
+//	// 現在のカメラ座標を代入
+//	mUIParentObj.position = Camera::sCurrent->position;
+//
+//}
+//
+//void SuperUI::Update()
+//{
+//
+//}
+//
+//void SuperUI::Draw()
+//{
+//
+//}
+//
+//void SuperUI::CopyComponent(IComponent* src)
+//{
+//
+//}
--- LibraTestProj/LibraTestDLL/SuperUI.h
+++ LibraTestProj/LibraTestDLL/SuperUI.h
@@ -1,39 +1,39 @@
-#pragma once
-#include "IScriptObject.h"
-#include <Object3D.h>
-
-class SuperUI :
-    public IScriptObject
-{
-public:
-    void Init();
-    void Update();
-    void Draw();
-    void CopyComponent(IComponent* src);
-
-    DefDel;
-
-private:
-    // メニューのステート
-
-
-private:// メンバー変数
-
-    // UI関連Allの親オブジェクト
-    Object3D mUIParentObj;
-
-    // UIのメインメニュー項目
-    std::vector<Object3D>mUIMainMenuObjs;
-
-    // UIのオプション項目
-    Object3D mUIOptionParentObj;
-    std::vector<Object3D> mUIOptionObjs;
-
-    // UIのオプションのそれぞれの項目
-    Object3D mUIOptionItemParentObj;
-    std::vector<Object3D> mUIOptionItemObjs;
-
-
-};
-RegisterScript(SuperUI);
+//#pragma once
+//#include "IScriptObject.h"
+//#include <Object3D.h>
+//
+//class SuperUI :
+//    public IScriptObject
+//{
+//public:
+//    void Init();
+//    void Update();
+//    void Draw();
+//    void CopyComponent(IComponent* src);
+//
+//    DefDel;
+//
+//private:
+//    // メニューのステート
+//
+//
+//private:// メンバー変数
+//
+//    // UI関連Allの親オブジェクト
+//    Object3D mUIParentObj;
+//
+//    // UIのメインメニュー項目
+//    std::vector<Object3D>mUIMainMenuObjs;
+//
+//    // UIのオプション項目
+//    Object3D mUIOptionParentObj;
+//    std::vector<Object3D> mUIOptionObjs;
+//
+//    // UIのオプションのそれぞれの項目
+//    Object3D mUIOptionItemParentObj;
+//    std::vector<Object3D> mUIOptionItemObjs;
+//
+//
+//};
+//RegisterScript(SuperUI);
 
