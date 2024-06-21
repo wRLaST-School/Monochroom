427f2130418e0534de110727010f3c4badcfed15
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2024-06-21 15:31:55 +0900

    UIベース作成途中。

---
+++ LibraTestProj/LibraTestDLL/Easing.cpp
@@ -0,0 +1,253 @@
+#include "Easing.h"
+#include <math.h>
+
+Easing::Easing() :
+	mTimer(0), mTimeRate(0),
+	mLimitTimer(0), mPowNum(1), mIsEnd(false)
+{
+}
+Easing::Easing(const int32_t easeTimer) :
+	mTimer(0), mTimeRate(0),
+	mLimitTimer(easeTimer), mPowNum(1), mIsEnd(false)
+{
+}
+Easing::Easing(const int32_t easeTimer, const float powNum) :
+	mTimer(0), mTimeRate(0),
+	mLimitTimer(easeTimer), mPowNum(powNum), mIsEnd(false)
+{
+}
+
+void Easing::Reset()
+{
+	mTimer = 0;
+	mTimeRate = 0;
+	mIsEnd = false;
+}
+
+void Easing::Update()
+{
+	mTimer++;
+	if (mTimeRate >= 1)
+	{
+		mIsEnd = true;
+	}
+
+	mTimer = Min<int32_t>(mTimer, mLimitTimer);
+	mTimeRate = Min<float>((float)mTimer / mLimitTimer, 1);
+}
+
+// セッター
+void Easing::SetEaseTimer(const int32_t limitTimer)
+{
+	mLimitTimer = limitTimer;
+}
+void Easing::SetPowNum(const float powNum)
+{
+	mPowNum = powNum;
+}
+void Easing::SetEaseType(const EaseType easeType)
+{
+	mEaseType = easeType;
+}
+void Easing::SetisEnd(const bool isEnd)
+{
+	mIsEnd = isEnd;
+}
+
+// ゲッター
+bool Easing::GetisEnd()
+{
+	return mIsEnd;
+}
+int32_t Easing::GetTimer()
+{
+	return mTimer;
+}
+float Easing::GetTimeRate()
+{
+	return mTimeRate;
+}
+uint32_t Easing::GetEaseTimer()
+{
+	return mLimitTimer;
+}
+
+// 補間
+float Easing::Interpolation(const float startPos, const float endPos)
+{
+	switch (mEaseType)
+	{
+	case EaseType::Lerp:
+		return Lerp(startPos, endPos);
+
+	case EaseType::In:
+		return In(startPos, endPos);
+
+	case EaseType::Out:
+		return Out(startPos, endPos);
+
+	case EaseType::InOut:
+		return InOut(startPos, endPos);
+
+	default:
+		return 0;
+	}
+}
+Vec2 Easing::Interpolation(const Vec2 startPos, const Vec2 endPos)
+{
+	switch (mEaseType)
+	{
+	case EaseType::Lerp:
+		return Lerp(startPos, endPos);
+
+	case EaseType::In:
+		return In(startPos, endPos);
+
+	case EaseType::Out:
+		return Out(startPos, endPos);
+
+	case EaseType::InOut:
+		return InOut(startPos, endPos);
+
+	default:
+		return Vec2(0, 0);
+	}
+}
+Vec3 Easing::Interpolation(const Vec3 startPos, const Vec3 endPos)
+{
+	switch (mEaseType)
+	{
+	case EaseType::Lerp:
+		return Lerp(startPos, endPos);
+
+	case EaseType::In:
+		return In(startPos, endPos);
+
+	case EaseType::Out:
+		return Out(startPos, endPos);
+
+	case EaseType::InOut:
+		return InOut(startPos, endPos);
+
+	default:
+		return Vec2(0, 0);
+	}
+}
+
+// ラープ
+float Easing::Lerp(const float startPos, const float endPos)
+{
+	float dis = endPos - startPos;
+	return dis * mTimeRate + startPos;
+}
+Vec2 Easing::Lerp(const Vec2 startPos, const Vec2 endPos)
+{
+	Vec2 dis = endPos - startPos;
+	return dis * mTimeRate + startPos;
+}
+Vec3 Easing::Lerp(const Vec3 startPos, const Vec3 endPos)
+{
+	Vec3 dis = endPos - startPos;
+	return dis * mTimeRate + startPos;
+}
+
+// イン
+float Easing::In(const float startPos, const float endPos)
+{
+	float dis = endPos - startPos;
+	return dis * powf(mTimeRate, mPowNum) + startPos;
+}
+Vec2 Easing::In(const Vec2 startPos, const Vec2 endPos)
+{
+	Vec2 dis = endPos - startPos;
+	return dis * powf(mTimeRate, mPowNum) + startPos;
+}
+Vec3 Easing::In(const Vec3 startPos, const Vec3 endPos)
+{
+	Vec3 dis = endPos - startPos;
+	return dis * powf(mTimeRate, mPowNum) + startPos;
+}
+
+// アウト
+float Easing::Out(const float startPos, const float endPos)
+{
+	float dis = endPos - startPos;
+	if ((int32_t)mPowNum % 2 == 1)
+	{
+		return dis * (powf(mTimeRate - 1, mPowNum) + 1) + startPos;
+	}
+	return dis * -1 * (powf(mTimeRate - 1, mPowNum) - 1) + startPos;
+}
+Vec2 Easing::Out(const Vec2 startPos, const Vec2 endPos)
+{
+	Vec2 dis = endPos - startPos;
+	if ((int32_t)mPowNum % 2 == 1)
+	{
+		return dis * (powf(mTimeRate - 1, mPowNum) + 1) + startPos;
+	}
+	return dis * -1 * (powf(mTimeRate - 1, mPowNum) - 1) + startPos;
+}
+Vec3 Easing::Out(const Vec3 startPos, const Vec3 endPos)
+{
+	Vec3 dis = endPos - startPos;
+	if ((int32_t)mPowNum % 2 == 1)
+	{
+		return dis * (powf(mTimeRate - 1, mPowNum) + 1) + startPos;
+	}
+	return dis * -1 * (powf(mTimeRate - 1, mPowNum) - 1) + startPos;
+}
+
+// インアウト
+float Easing::InOut(const float startPos, const float endPos)
+{
+	float dis = (endPos - startPos);
+
+	if (mTimeRate < 0.5f)
+	{
+		return dis / 2.f * powf(mTimeRate * 2.f, mPowNum) + startPos;
+	}
+	return -dis / 2.f * (powf((1.f - (mTimeRate - 0.5f) * 2.f), mPowNum) - 2.f) + startPos;
+
+}
+Vec2 Easing::InOut(const Vec2 startPos, const Vec2 endPos)
+{
+	Vec2 dis = (endPos - startPos) / 2;
+	if (mTimeRate < 0.5f)
+	{
+		return In(startPos, endPos);
+	}
+	return Out(startPos, endPos);
+}
+Vec3 Easing::InOut(const Vec3 startPos, const Vec3 endPos)
+{
+	Vec3 dis = (endPos - startPos);
+
+	if (mTimeRate < 0.5f)
+	{
+		return dis / 2.f * powf(mTimeRate * 2.f, mPowNum) + startPos;
+	}
+	return -dis / 2.f * (powf((1.f - (mTimeRate - 0.5f) * 2.f), mPowNum) - 2.f) + startPos;
+}
+
+// インバック
+float Easing::InBack(const float startPos, const float endPos)
+{
+	const float back1 = 1.70154f;
+
+	float dis = endPos - startPos;
+	return dis * powf(mTimeRate, mPowNum) * ((back1 + 1.0f) * mTimeRate - back1) + startPos;
+}
+Vec2 Easing::InBack(const Vec2 startPos, const Vec2 endPos)
+{
+	const float back1 = 1.70154f;
+
+	Vec2 dis = endPos - startPos;
+	return dis * powf(mTimeRate, mPowNum) * ((back1 + 1.0f) * mTimeRate - back1) + startPos;
+}
+Vec3 Easing::InBack(const Vec3 startPos, const Vec3 endPos)
+{
+	const float back1 = 1.70154f;
+
+	Vec3 dis = endPos - startPos;
+	return dis * powf(mTimeRate, mPowNum) * ((back1 + 1.0f) * mTimeRate - back1) + startPos;
+}
--- LibraTestProj/LibraTestDLL/SuperUI.h
+++ LibraTestProj/LibraTestDLL/SuperUI.h
@@ -1,39 +1,83 @@
-//#pragma once
-//#include "IScriptObject.h"
-//#include <Object3D.h>
-//
-//class SuperUI :
-//    public IScriptObject
-//{
-//public:
-//    void Init();
-//    void Update();
-//    void Draw();
-//    void CopyComponent(IComponent* src);
-//
-//    DefDel;
-//
-//private:
-//    // メニューのステート
-//
-//
-//private:// メンバー変数
-//
-//    // UI関連Allの親オブジェクト
-//    Object3D mUIParentObj;
-//
-//    // UIのメインメニュー項目
-//    std::vector<Object3D>mUIMainMenuObjs;
-//
-//    // UIのオプション項目
-//    Object3D mUIOptionParentObj;
-//    std::vector<Object3D> mUIOptionObjs;
-//
-//    // UIのオプションのそれぞれの項目
-//    Object3D mUIOptionItemParentObj;
-//    std::vector<Object3D> mUIOptionItemObjs;
-//
-//
-//};
-//RegisterScript(SuperUI);
+#pragma once
+#include "IScriptObject.h"
+#include <Object3D.h>
+#include "Easing.h"
+
+class SuperUI :
+    public IScriptObject
+{
+public:
+    void Init();
+    void Update();
+    void Draw();
+    void CopyComponent(IComponent* src);
+
+    DefDel;
+
+private:// クラス内構造体
+
+    struct UIMenuStatus
+    {
+        // ベースの座標
+        Vec2 basePos;
+
+        // 出現時に変化するスケール量
+        Vec2 scaleChangeValue;
+
+        // 今アクティブ状態かどうか
+        bool IsActiveMenu;
+
+        // タイミングがずれた時
+        bool IsTimingOff;
+    };
+
+private:
+    // メニューのステート
+
+    // 親子関係の設定
+    void LoadTexInit();
+
+    // メニューが開いた瞬間のリセット
+    void MomentMenuReset();
+
+private:// メンバー変数
+
+    // UIメニューを開くかどうか
+    bool mIsOpenUIMenu;
+
+    // UIメニューの開いた瞬間
+    bool mIsMomentOpenMenu;
+
+    // オプションメイン項目の親座標
+    Vec2 mUiOptionBasePos;
+
+    // オプションそれぞれの項目の親座標
+    Vec2 mUiOptionItemBasePos;
+
+    // イージング
+    std::vector<Easing> mEaseUIMenu;
+
+    // UIの項目数
+    int32_t mNumOfUIOption;
+
+    // UIのメインメニュー項目
+    std::vector<Object3D*>mUIMainMenuObjs;
+
+    // UIの変化するスケール配列
+    std::vector<UIMenuStatus> mUIStatus;
+
+    int mUiWaitTimeInterval = 20;
+    int mUiWaitTimer = 0;
+
+    // UIのオプション項目
+    Object3D* mUIOptionParentObj;
+    std::vector<Object3D*> mUIOptionObjs;
+
+    // UIのオプションのそれぞれの項目
+    Object3D* mUIOptionItemParentObj;
+    std::vector<Object3D*> mUIOptionItemObjs;
+
+
+};
+RegisterScript(SuperUI);
 
