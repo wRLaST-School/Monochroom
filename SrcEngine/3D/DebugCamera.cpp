2c3861f2006b853f08bccb5cc07811aae159c125
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2024-06-22 14:08:20 +0900

    DebugCameraのショートカットを追加 Shift+R：リセット Shift+X：X軸から見る Shift+Y：Y軸から見る Shift+Z：Z軸から見る

--- SrcEngine/3D/DebugCamera.cpp
+++ SrcEngine/3D/DebugCamera.cpp
@@ -99,23 +99,44 @@ void DebugCamera::Update()
 		debugCamera->position += vec * Mouse::GetWheelVel() * moveSpeed;
 	}
 
-	if (Key::Down(DIK_LSHIFT) && Key::Down(DIK_C))
+	if (Key::Down(DIK_LSHIFT))
 	{
-		target = Vec3::zero;
-		debugCamera->position = Vec3(0, 0, -10);
-		debugCamera->rotationE = Vec3(0, 0, 0);
-		yawAngle = 0;
-		pitchAngle = 0;
-	}
-	if (Key::Down(DIK_LSHIFT) && Key::Down(DIK_T))
-	{
-		target = Vec3::zero;
-		debugCamera->position = Vec3(0, 0, -10);
-		debugCamera->rotationE = Vec3(ConvertAngleToRadian(45), 0, 0);
-		yawAngle = 0;
-		pitchAngle = 0;
-	}
+		Vec3 diff = Vec3(debugCamera->position) - target;
 
+		if (Key::Triggered(DIK_R))
+		{
+			target = Vec3::zero;
+			debugCamera->position = Vec3(0, 0, -10);
+			debugCamera->rotationE = Vec3(0, 0, 0);
+			yawAngle = 0;
+			pitchAngle = 0;
+		}
+		else if (Key::Triggered(DIK_X))
+		{
+			target = Vec3::zero;
+			debugCamera->position = Vec3::right * diff.GetLength();
+			debugCamera->rotationE = Vec3(0, -ConvertAngleToRadian(90), 0);
+			yawAngle = -90;
+			pitchAngle = 0;
+		}
+		else if (Key::Triggered(DIK_Y))
+		{
+			target = Vec3::zero;
+			debugCamera->position = Vec3::up * diff.GetLength();
+			debugCamera->rotationE = Vec3(ConvertAngleToRadian(90), 0, 0);
+			yawAngle = 0;
+			pitchAngle = 90;
+		}
+		else if (Key::Triggered(DIK_Z))
+		{
+			target = Vec3::zero;
+			debugCamera->position = Vec3::front * diff.GetLength();
+			debugCamera->rotationE = Vec3(0, -ConvertAngleToRadian(180), 0);
+			yawAngle = -180;
+			pitchAngle = 0;
+		}
+
+	}
 	debugCamera->UpdateMatrix();
 	Camera::Set(*debugCamera.get());
 }
