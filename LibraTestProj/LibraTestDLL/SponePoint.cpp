737980f1a5fbf578200dc7dc6481ea0fae7e004d
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2024-07-11 00:55:45 +0900

    同じシーンの読み込みだとなんか動かない

--- LibraTestProj/LibraTestDLL/SponePoint.cpp
+++ LibraTestProj/LibraTestDLL/SponePoint.cpp
@@ -1,8 +1,9 @@
 #include "SponePoint.h"
 #include <ScriptComponent.h>
 #include <SceneManager.h>
+#include <SceneFromFile.h>
+#include <IComponent.h>
 #include "AppOperationCommand.h"
-
 
 void SponePoint::Init()
 {
@@ -13,10 +14,16 @@ void SponePoint::Init()
 
 void SponePoint::Update()
 {
-	if (AppOperationCommand::GetInstance()->ReStartCommand())
-	{
-		SponePlayer();
-	}
+	//if (AppOperationCommand::GetInstance()->ReStartCommand())
+	//{
+	//	//IComponent::InitAllChildComponents(SceneManager::GetCurrentScene());
+	//	// シーンの切り替え処理
+	//	std::string sceneName = SceneManager::currentScene->GetName();
+	//	SceneManager::LoadScene<SceneFromFile>("Assets/Scene/" + sceneName + ".scene");
+	//	SceneManager::WaitForLoadAndTransition();
+
+	//	//SponePlayer();
+	//}
 }
 
 void SponePoint::Draw()
@@ -29,7 +36,7 @@ void SponePoint::Draw()
 void SponePoint::SponePlayer()
 {
 	auto player = SceneManager::FindObject<Object3D>("Player");
-	
+
 	player->position = mParent->position;
 	player->rotationE = mParent->rotationE;
 }
