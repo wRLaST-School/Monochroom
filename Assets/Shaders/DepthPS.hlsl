0e641545f84312a63906bb75ed0b4e161f010859
Wrelf <elf@wrelf.net>
2024-07-13 14:26:59 +0900

    depth textureを見やすく

--- Assets/Shaders/DepthPS.hlsl
+++ Assets/Shaders/DepthPS.hlsl
@@ -6,6 +6,9 @@ SamplerState smp : register(s0);
 float4 main(VSOutput input) : SV_TARGET
 {
     float4 result = tex.Sample(smp, input.uv) * color;
+    result.r = result.r * result.r * result.r * result.r * result.r;
+    result.r *= result.r;
+    result.r *= result.r;
     result.a = 1;
     
     return result;
