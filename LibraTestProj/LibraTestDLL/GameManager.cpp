a2504fe537f55496d4b6f18b71da33dc35d9ea87
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2024-08-03 22:15:33 +0900

    ステージにリセット入れた

--- LibraTestProj/LibraTestDLL/GameManager.cpp
+++ LibraTestProj/LibraTestDLL/GameManager.cpp
@@ -140,6 +140,15 @@ void GameManager::Update()
 			}
 		}
 
+		// リセット
+		std::string sceneName = SceneManager::GetCurrentScene()->GetTag(0);
+		if (AppOperationCommand::GetInstance()->ReStartCommand())
+		{
+			BlinkTransition::mReset = true;
+			BlinkTransition::Start();
+			BlinkTransition::mIsChangeScene = true;
+		}
+
 		if (BlinkTransition::mIsChangeScene)
 		{
 			if (BlinkTransition::info.isInEnd)
@@ -151,19 +160,17 @@ void GameManager::Update()
 					SceneManager::WaitForLoadAndTransition();
 					BlinkTransition::mToTitle = false;
 				}
-				else
+				else if (BlinkTransition::mReset)
 				{
-					// リセット
-					std::string sceneName = SceneManager::GetCurrentScene()->GetName();
-					if (AppOperationCommand::GetInstance()->ReStartCommand())
-					{
-						// シーンの切り替え処理
-						SceneManager::LoadScene<SceneFromFile>("Assets/Scene/Stage/" + sceneName + ".scene");
-						SceneManager::WaitForLoadAndTransition();
-					}
+					// シーンの切り替え処理
+					SceneManager::LoadScene<SceneFromFile>("Assets/Scene/Stage/" + sceneName + ".scene");
+					SceneManager::WaitForLoadAndTransition();
+					BlinkTransition::mReset = false;
 				}
 			}
 		}
+
+
 
 		if (!mStageGenerater)
 		{
