7974f64b1ccfc1fb74f1f755c9c5b37f326dbd94
Wrelf <elf@wrelf.net>
2024-08-03 20:37:01 +0900

    リリース起動の設定を変更、一旦リリースビルド不可

--- SrcApplication/GameManager.h
+++ SrcApplication/GameManager.h
@@ -4,5 +4,9 @@ class GameManager {
 public:
 	static bool sShowDebug;
 	static bool sShowHitBoxes;
+#ifdef _DEBUG
 	inline static bool sDebugTimeStop = true;
+#else
+	inline static bool sDebugTimeStop = false;
+#endif
 };
