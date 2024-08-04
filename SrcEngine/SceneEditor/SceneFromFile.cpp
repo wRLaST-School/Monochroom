1c4361f1525c6381e4003f38085bed6f8e14e1e4
Wrelf <elf@wrelf.net>
2024-08-04 14:35:34 +0900

    サウンド入れ

--- SrcEngine/SceneEditor/SceneFromFile.cpp
+++ SrcEngine/SceneEditor/SceneFromFile.cpp
@@ -18,12 +18,15 @@ void SceneFromFile::LoadResources()
 {
 	SceneRW::LoadScene(this, filePath_);
 
-	SoundManager::LoadWave("Assets/Sounds/blink.wav", "Blink");
-	SoundManager::LoadWave("Assets/Sounds/decide.wav", "Decide");
-	SoundManager::LoadWave("Assets/Sounds/generate.wav", "Generate");
-	SoundManager::LoadWave("Assets/Sounds/select.wav", "Select");
-	SoundManager::LoadWave("Assets/Sounds/goal.wav", "Goal");
-}
+	SoundManager::LoadWave("Assets/Sounds/blink.wav", "Blink"); // トランジション
+	SoundManager::LoadWave("Assets/Sounds/decide.wav", "Decide"); // 決定音
+	SoundManager::LoadWave("Assets/Sounds/generate.wav", "Generate"); // ステージ生成音
+	SoundManager::LoadWave("Assets/Sounds/select.wav", "Select"); // 選択(上下左右)音
+	SoundManager::LoadWave("Assets/Sounds/goal.wav", "Goal"); // ゴールしたとき
+	SoundManager::LoadWave("Assets/Sounds/goggle.wav", "Goggle"); // ゴーグルつけたとき
+	SoundManager::LoadWave("Assets/Sounds/kinesis.wav", "Kinesis"); // 引き寄せるとき
+	SoundManager::LoadWave("Assets/Sounds/presplate.wav", "PresPlate"); // ステージのボタンに乗った時
+	SoundManager::LoadWave("Assets/Sounds/jump.wav", "Jump"); // ジャンプ時
 
 void SceneFromFile::Init()
 {
